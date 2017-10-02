9a2367e53178faad072da8824faf380f27cfe177
Chuck Konkol <ckonkol@gmail.com>
2017-10-02 16:03:46 -0500

    ck

---
+++ MyBand1/DetailViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailViewController.swift
+//  MyBand1
+//
+//  Created by cis290 on 10/2/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+//********1) Remove label from DetailViewController
+//            -  add an ImageView
+//            -  create outlet of imageview and name productImageView
+    
+//********2) Comment out (1) line below
+  //  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+    @IBOutlet weak var productImageView: UIImageView!
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if detailItem != nil {
+//********3) Update (6) lines of code below
+            if let photo = productImageView {
+                photo.image = UIImage(named:detailItem!)
+            }
+        }else{
+             productImageView.image = UIImage(named:"Main.png")
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+//********4) Update next line of code below
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ MyBand1/MasterViewController.swift
@@ -0,0 +1,108 @@
+//
+//  MasterViewController.swift
+//  MyBand1
+//
+//  Created by cis290 on 10/2/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+//********1) comment out below
+    //var objects = [Any]()
+     var listArray = NSMutableArray()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+ //*******2) comment out below
+//        navigationItem.leftBarButtonItem = editButtonItem
+//
+//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+//        navigationItem.rightBarButtonItem = addButton
+//********3) Add next two lines for array and title
+         listArray = ["Joe Cocker","Leonard Skynard","Eagles","Journey", "38 Special","Miranda Lambert", "Little Big Town","Band Perry"]
+         title = "My Favorite Bands"
+        if let split = splitViewController {
+            let controllers = split.viewControllers
+            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+//********4) comment out below
+//    func insertNewObject(_ sender: Any) {
+//        objects.insert(NSDate(), at: 0)
+//        let indexPath = IndexPath(row: 0, section: 0)
+//        tableView.insertRows(at: [indexPath], with: .automatic)
+//    }
+
+    // MARK: - Segues
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+//********5) Update below 6 lines of code
+                let object = listArray[indexPath.row] as! String
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = (object as String) + ".jpg"
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+                self.splitViewController?.preferredDisplayMode = .primaryHidden
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//********6) Update below (1) line
+        return listArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//********7) Update below (1) line
+        let object = listArray[indexPath.row] as! NSString
+        cell.textLabel!.text = object.description
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+//********8) Update below (1) line
+        return false
+    }
+
+    
+//********9) comment out below
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            objects.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        } else if editingStyle == .insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+//        }
+//    }
+
+
+}
+
